39af1c52280360b9067074d80a9e23570b094446
kenta66448284 <92067021+kenta66448284@users.noreply.github.com>
2022-08-04 10:08:58 +0900

    基本的設定

---
+++ pokemon.js
@@ -0,0 +1,83 @@
+
+let number = document.querySelector(".number");
+let submit = document.querySelector(".submit");
+let tipecolor = {
+    normal:["ノーマル","#A8A878"],
+    fire:["ほのお","#F08030"],
+    water:["みず","#6890F0"],
+    grass:["くさ","#78C850"],
+    electric:["でんき","#F8D030"],
+    ice:["こおり","#98D8D8"],
+    fighting:["かくとう","#C03028"],
+    poison:["どく","#A040A0"],
+    ground:["じめん","#E0C068"],
+    flying:["ひこう","#A890F0"],
+    psychic:["エスパー","#F85888"],
+    bug:["むし","#A8B820"],
+    rock:["いわ","#B8A038"],
+    ghost:["ゴースト","#705898"],
+    dragon:["ドラゴン","#7038F8"],
+    dark:["あく","#705848"],
+    steel:["はがね","#B8B8D0"],
+    fairy:["フェアリー","#EE99AC"],
+};
+let list = document.querySelector(".pokemonlist");
+let ul = document.createElement("ul");
+
+console.log(list)
+for (let i = 0;i<899;i++){
+    const listurl = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let li = document.createElement("li");
+    li.classList.add("pokelist")
+    let link = document.createElement("a");
+    let listimg = document.createElement("img");
+    let p = document.createElement("p");
+    fetch(listurl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(list){
+        let listname = list.name;
+        listimg.setAttribute("src",list.sprites.front_default)
+        p.innerHTML=listname;
+        li.appendChild(p);
+        li.appendChild(listimg)
+        ul.appendChild(li)
+
+    })
+}
+list.appendChild(ul)
+
+
+submit.addEventListener("click",function(){
+    let pokenumber = number.value;
+    let namenumber = pokenumber -1;
+    console.log(namenumber)
+    console.log(pokenumber);
+    // let janame = pokemonname[namenumber]["ja"]
+    let janame = pokemonname[namenumber]["ja"]
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokenumber}`;
+    fetch(url)
+    .then(function(response){
+        return response.json();
+
+    })
+    .then(function(deta){
+        console.log(deta)
+        console.log(deta.name);
+        let name = document.querySelector(".pokemon_name");
+        let img = document.querySelector(".pokemon_img");
+        name.innerHTML=janame;
+        img.setAttribute("src",deta.sprites.front_default);
+        let ul = document.querySelector(".pokemon_type");
+        ul.innerHTML = "";
+        for(let i = 0;i<deta.types.length;i++){
+            let li = document.createElement("li");
+            let colorname= deta.types[i].type.name;
+            console.log(colorname);
+            li.innerHTML = tipecolor[colorname][0];
+            li.setAttribute("style","background-color:"+tipecolor[colorname][1])
+            ul.appendChild(li);
+        }
+    })
+})
